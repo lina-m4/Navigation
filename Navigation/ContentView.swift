81724411c6d58a5ff2f9861a8aa9096f49391f6e
Lina <linamered3@gmail.com>
2025-07-24 12:59:25 -0400

    Initial Commit

---
+++ Navigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by Lina Mered on 7/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
