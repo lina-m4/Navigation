d76cadc02682c0b203463f531296380b5fcc2d66
Lina <linamered3@gmail.com>
2025-07-24 13:42:40 -0400

    Added padding for more seamless UI

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -11,19 +11,23 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                Text("This is the home page")
+                Text("This is the home page!")
+                    .padding()
                                 
                 NavigationLink(destination: AboutView()) {
                     Text("About")
                 }
+                .padding()
 
                 NavigationLink(destination: Contact()) {
                     Text("Contact")
                 }
+                .padding()
 
                 NavigationLink(destination: Help()) {
                     Text("Help")
                 }
+                .padding()
 
             }
             .navigationTitle("Home")
